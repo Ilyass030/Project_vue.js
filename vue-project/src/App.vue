37d14fd2278961089d2fddb07c0ef9d2fa4ca323
Remmane <ilyass.remmane@edu.esiee.fr>
2026-02-09 20:32:37 +0100

    test_dog

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,22 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <h1>Dog Center</h1>
+    <div id="dog-gallery">
+      <DogCard
+        v-for="dog in dogsData"
+        :key="dog.id"
+        :name="dog.name"
+        :age="dog.age"
+        :breed="dog.breed"
+        :pictureUrl="dog.pictureUrl"
+        :soundUrl="dog.soundUrl"
+      />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import dogsData from './dogsData'     
+import DogCard from './components/DogCard.vue'
+import DogCard from './components/NewsSlider.vue'
+</script>
